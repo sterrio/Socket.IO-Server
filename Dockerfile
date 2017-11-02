0fbb0913bcf5921e2f7c23c893e7b60a12e275e5
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2017-11-02 09:34:21 -0300

    Configured socket.io servers listeners and emits properly

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM node:8.9
+FROM node:8
 WORKDIR /app
 COPY package.json /app
 RUN npm install
-COPY ./app
+COPY . /app
 CMD node main.js
-EXPOSE 80
+EXPOSE 8081
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var app = require('http').createServer()
 var io = require('socket.io')(app);
 var fs = require('fs');
 
-app.listen(6000);
-
+app.listen(8080);
+console.log('running!')
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  // store username in socket obj
+  socket.username = socket.handshake.query['user']
+  socket.emit('joined', { user: socket.username });
+  socket.on('message', function (data) {
+    socket.broadcast.emit('message_update', {user: socket.username, msg: data})
   });
 });
